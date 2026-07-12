5417f9aa4fc233558477bd542d0ddff65e9bcf44
letematik <letematik@gmail.com>
2026-07-12 02:51:22 -0400

    Backup: visual carousel and media folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://letematik.github.io',
-  base: '/the-difference',
   vite: {
     plugins: [tailwindcss()]
   }
